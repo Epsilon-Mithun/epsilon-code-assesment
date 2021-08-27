64ade52cbb6598c12ff69ab5768caae5ad00a3df
Mithun Chide <mithunchide03@gmail.com>
2021-08-27 10:54:06 +0530

    some changes in cameras components

--- src/components/Cameras/Camera.js
+++ src/components/Cameras/Camera.js
@@ -6,14 +6,14 @@ return (
     <header>
         <h1>Cameras</h1>
         <div className="buttons">
-        <button type="button" >Primary</button>
-        <button type="button" >Primary</button>
-        <button type="button" >Primary</button>
-        <button type="button" >Primary</button>
-        <button type="button" >Primary</button>
-        <button type="button" >Primary</button>
-        <button type="button" >Primary</button>
-        <button type="button" >Primary</button>
+        <button type="button" >Instant</button>
+        <button type="button" >DBLR</button>
+        <button type="button" >Convenience Secuirity and Collision Avoidance</button>
+        <button type="button" >Action</button>
+        <button type="button" >Camcorders</button>
+        <button type="button" >Drones</button>
+        <button type="button" >Super Mega Pixel Ultimate Zoom</button>
+        <button type="button" >Wearables</button>
         </div>
     </header>
     );
